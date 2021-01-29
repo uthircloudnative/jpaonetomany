704df870352c2cff795074c21decaad949f9987a
uthircloudnative <uthircloudnative@gmail.com>
2021-01-29 16:33:30 -0500

    Added new Controller for UniDirctional functionality

---
+++ src/main/java/com/gmdb/jpaonetomany/unidirectionalentymapping/controller/UniDirOneToManyController.java
@@ -0,0 +1,9 @@
+package com.gmdb.jpaonetomany.unidirectionalentymapping.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UniDirOneToManyController {
+
+
+}
